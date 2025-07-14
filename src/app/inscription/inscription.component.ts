d7b526e80436acf754b38757cc10949dc3a15965
thesenegalesehitch <aa.ndour5@isepat.edu.sn>
2025-07-14 09:26:46 +0000

    Ajout des validations pour les champs concernant l'inscription

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-inscription',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.scss'
 })
 export class InscriptionComponent {
+inscriptionForm : FormGroup=new FormGroup({
 
+  prenom:new FormControl('', [Validators.required]),
+  nom:new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]),
+  email:new FormControl('', [Validators.required, Validators.email]),
+  password:new FormControl('', [Validators.required]),
+  adresse:new FormControl('Diamniadio', [Validators.required])
+
+
+})
 }
